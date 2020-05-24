7379bde41bf14dd80d20060f53880367e9d98b9f
ianmeesters <46504388+ianmeesters@users.noreply.github.com>
2020-05-24 11:13:50 +0200

    nieuwe

---
+++ Cinema/Zalen.cs
@@ -0,0 +1,34 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace Cinema
+{
+    public class Zalen
+    {
+        public class Zaal
+        {
+            public string nummer { get; set; }
+            public List<List<string>> stoelen { get; set; }
+            public string date { get; set; }
+            public string startTime { get; set; }
+            public string endTime { get; set; }
+        }
+
+        public static void NieuweZaal()
+        {
+
+        }
+
+        public static void zalen()
+        {
+            var zalen = JsonConvert.DeserializeObject<Dictionary<string, List<string>>>(File.ReadAllText(@"users.json"));
+        }
+
+
+    }
+}
