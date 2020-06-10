591abf47f27691603279c013ee7d065019c93e16
ianmeesters <46504388+ianmeesters@users.noreply.github.com>
2020-06-10 14:45:58 +0200

    stoel

--- Cinema/Program.cs
+++ Cinema/Program.cs
@@ -26,9 +26,8 @@ namespace Cinema
         public static void Main(string[] args)
         {
             Zalen.removedStoelen("27/05/2020", "11:00");
-            // Calendar.runCalendar();
-            // Mainmenu koppelen
-            // Mainmenu.Menu();
+            //Calendar.runCalendar();
+            //Mainmenu.Menu();
         }
     }
 }
