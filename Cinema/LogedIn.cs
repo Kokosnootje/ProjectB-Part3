587b62e512cfc2829ce107d455f6c68c3087c051
BryanDoel <33514386+BryanDoel@users.noreply.github.com>
2020-06-11 19:12:42 +0200

    Kleine update

--- Cinema/LogedIn.cs
+++ Cinema/LogedIn.cs
@@ -351,13 +351,13 @@ namespace Cinema
                                     }
                                     delAnswer = true;
                                     resAnswer = true;
-                                    LogedInMain();
+                                    LogedInAdmin();
                                 }
                                 else if (confirmDelete == "2")
                                 {
                                     delAnswer = true;
                                     resAnswer = true;
-                                    LogedInMain();
+                                    LogedInAdmin();
                                 }
                                 else
                                 {
